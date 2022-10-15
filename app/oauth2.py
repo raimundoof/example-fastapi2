ef39f7fcde4974b5047a1f47a7eef660bcd4f520
raimundoof <raimundo.falcao@gmail.com>
2022-10-14 22:35:39 -0300

    sem tk2

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,4 +1,4 @@
-from tkinter.messagebox import RETRY
+#from tkinter.messagebox import RETRY
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schemas, database, models
